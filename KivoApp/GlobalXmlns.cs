8b137793cde7dbfc56b10ade4938e45c93a83574
GabrielFMartins1009 <103799@aluno.uricer.edu.br>
2025-09-25 20:22:18 -0300

    Add project files.

---
+++ KivoApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KivoApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KivoApp.Pages")]
---
+++ KivoApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace KivoApp
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
